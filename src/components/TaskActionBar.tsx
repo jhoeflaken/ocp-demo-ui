bbd34c60cbf29bb65dbb9ce645bfae81a03922f4
jhoeflak <jacob.hoeflaken@axians.com>
2021-11-09 18:24:54 +0100

    Updated UI.

--- src/components/TaskActionBar.tsx
+++ src/components/TaskActionBar.tsx
@@ -1,16 +1,50 @@
 import * as React from "react";
-import AddIcon from "@mui/icons-material/Add";
-import DeleteIcon from "@mui/icons-material/Delete";
 import {IconButton} from "@mui/material";
+import {
+    AddOutlined,
+    DeleteOutlined,
+    ErrorOutline,
+    PanToolOutlined,
+    StorageOutlined
+} from "@mui/icons-material";
+import ActionService from "../services/ActionService";
 
 export const TaskActionBar = () => {
+    const handleThrowError = () => {
+        ActionService.throwError()
+            .then((response: any) => {
+                console.log(response)
+            })
+            .catch((error: Error) => {
+                console.log(error)
+            });
+    }
+
+    const handleCrashApplication = () => {
+        ActionService.crashApplication().then();
+    }
+
+    const handleInitialize = () => {
+        ActionService.initialize().then();
+    }
+
+
     return (
         <div style={{margin: "8px 8px", textAlign: "left"}}>
             <IconButton color="primary" aria-label="add">
-                <AddIcon />
+                <AddOutlined />
             </IconButton>
             <IconButton color="primary" aria-label="delete">
-                <DeleteIcon />
+                <DeleteOutlined />
+            </IconButton>
+            <IconButton color="primary" onClick={handleInitialize} aria-label="initialize">
+                <StorageOutlined />
+            </IconButton>
+            <IconButton color="primary" onClick={handleThrowError} aria-label="error">
+                <ErrorOutline />
+            </IconButton>
+            <IconButton color="primary" onClick={handleCrashApplication} aria-label="crash">
+                <PanToolOutlined />
             </IconButton>
         </div>
     )
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,44 +1,10 @@
-import {AppBar, IconButton, Menu, MenuItem, Toolbar} from "@mui/material";
+import {AppBar, IconButton, Toolbar} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import Button from "@mui/material/Button";
-import React from "react";
-import ActionService from "../services/ActionService";
+import * as React from "react";
 
 export const TopBar = () => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-
-    const handleClick = (event: any) => {
-        setAnchorEl(event.currentTarget);
-    }
-
-    const handleThrowError = () => {
-        setAnchorEl(null);
-        ActionService.throwError()
-            .then((response: any) => {
-                console.log(response)
-            })
-            .catch((error: Error) => {
-                console.log(error)
-            });
-    }
-
-    const handleCrashApplication = () => {
-        setAnchorEl(null);
-        ActionService.crashApplication().then();
-    }
-
-    const handleInitialize = () => {
-        setAnchorEl(null);
-        ActionService.initialize().then();
-    }
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    }
-
     return (
-        <AppBar position="static">
+        <AppBar position="static" color={"primary"}>
             <Toolbar>
                 <IconButton
                     size="large"
@@ -49,29 +15,7 @@ export const TopBar = () => {
                 >
                     <MenuIcon />
                 </IconButton>
-                <Button
-                    id="btnActions"
-                    aria-controls="basic-menu"
-                    aria-haspopup="true"
-                    aria-expanded={open ? "true" : undefined}
-                    onClick={handleClick}
-                    color="inherit"
-                >
-                    Actions
-                </Button>
-                <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                        "aria-labelledby": "basic-button",
-                    }}
-                >
-                    <MenuItem onClick={handleThrowError}>Throw error</MenuItem>
-                    <MenuItem onClick={handleCrashApplication}>Crash application</MenuItem>
-                    <MenuItem onClick={handleInitialize}>Initialize</MenuItem>
-                </Menu>
+                <span>Magnificent Task v0.10</span>
             </Toolbar>
         </AppBar>
     )
